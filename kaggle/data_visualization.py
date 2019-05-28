a69c13a8a8ac2cefbf0d40851ed535ee475194c7
Marcio Lima <marcio.lima.inacio@gmail.com>
2019-05-28 18:28:16 -0300

    Visualização do balanceamento das classes

--- kaggle/data_visualization.py
+++ kaggle/data_visualization.py
@@ -1,4 +1,4 @@
-#! /usr/env/ python3
+#!/usr/bin/env python3
 import pandas as pd
 from matplotlib import pyplot as plt
 
@@ -15,6 +15,14 @@ def main():
     df = pd.read_csv('data/train.csv')
     features = df.columns
 
+    # Class balance
+    class_balance = df.groupby('target')['target'].count()
+    plt.figure(figsize=(8, 8))
+    plt.pie(class_balance)
+    plt.legend(class_balance.index)
+    plt.show()
+
+    # Correlation matrix
     corr_features = {f for f in features if not f.endswith('_cat') and not f.endswith('_bin')}
     corr_features.remove('id')
     corr(df, corr_features)
