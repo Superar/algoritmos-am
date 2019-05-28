b6b998c63ee8889af9926b371ffd6b641aee12ce
Marcio Lima <marcio.lima.inacio@gmail.com>
2019-05-28 18:01:08 -0300

    Vizualização da matriz de correlação

---
+++ kaggle/data_visualization.py
@@ -0,0 +1,23 @@
+#! /usr/env/ python3
+import pandas as pd
+from matplotlib import pyplot as plt
+
+
+def corr(data, features):
+    # Correlation between values
+    corr_mat = data.loc[:, features].corr()
+    plt.matshow(corr_mat)
+    plt.xticks(range(len(corr_mat.columns)), corr_mat.columns, rotation=90)
+    plt.yticks(range(len(corr_mat.columns)), corr_mat.columns)
+    plt.show()
+
+def main():
+    df = pd.read_csv('data/train.csv')
+    features = df.columns
+
+    corr_features = {f for f in features if not f.endswith('_cat') and not f.endswith('_bin')}
+    corr_features.remove('id')
+    corr(df, corr_features)
+
+if __name__ == "__main__":
+    main()
