46ca2f5fcbd30e2b02a33799283d5a984f4f65b7
Marcio Lima <marcio.lima.inacio@gmail.com>
2019-06-14 14:04:46 -0300

    Mudanças noo clustering do Kaggle - Seleção de apenas 10% dos dados para o clustering - Retirada a divisão em treino e teste

--- kaggle/clustering.py
+++ kaggle/clustering.py
@@ -2,45 +2,44 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.cluster import MiniBatchKMeans, AgglomerativeClustering
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import homogeneity_completeness_v_measure
-from sklearn.metrics import confusion_matrix
 
 
 def run_experiment(model, data):
     X = data.drop('target', axis=1)
     y = data.loc[:, 'target']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
     # Training
-    model.fit(X_train)
-    homo, comp, v_m = homogeneity_completeness_v_measure(y_train,
-                                                         model.labels_)
-    print('Training Homogeneity: {:.2f}%'.format(homo * 100))
-    print('Training Completeness: {:.2f}%'.format(comp * 100))
-    print('Training V-Measure: {:.2f}%'.format(v_m * 100))
-
-    # Test
-    results = model.predict(X_test)
-    homo, comp, v_m = homogeneity_completeness_v_measure(y_test,
-                                                         results)
-    print('\nTesting Homogeneity: {:.2f}%'.format(homo * 100))
-    print('Testing Completeness: {:.2f}%'.format(comp * 100))
-    print('Testing V-Measure: {:.2f}%'.format(v_m * 100))
+    y_pred = model.fit_predict(X)
+    homo, comp, v_m = homogeneity_completeness_v_measure(y,
+                                                         y_pred)
+    print('Homogeneity: {:.2f}%'.format(homo * 100))
+    print('Completeness: {:.2f}%'.format(comp * 100))
+    print('V-Measure: {:.2f}%'.format(v_m * 100))
 
 
 def main():
     df = pd.read_csv('data/train.csv')
     data = df.drop('id', axis=1)
 
-    # KMeans
-    print(5*'-', 'KMeans', 5*'-')
+    # KMeans - 100%
+    print(5*'-', 'KMeans - 100%', 5*'-')
     model = MiniBatchKMeans(n_clusters=100)
     run_experiment(model, data)
     print(20*'-')
 
-    # Agglomerative Clustering
-    print(5*'-', 'Agglomerative Clustering', 5*'-')
+    # KMeans - 10%
+    df = df.groupby('target').apply(pd.DataFrame.sample,
+                                    frac=0.1).reset_index(drop=True)
+    data = df.drop('id', axis=1)
+
+    print(5*'-', 'KMeans - 10%', 5*'-')
+    model = MiniBatchKMeans(n_clusters=100)
+    run_experiment(model, data)
+    print(20*'-')
+
+    # Agglomerative Clustering - 10%
+    print(5*'-', 'Agglomerative Clustering - 10%', 5*'-')
     model = AgglomerativeClustering(n_clusters=2)
     run_experiment(model, data)
     print(30*'-')
